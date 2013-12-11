be1273a37c494bf143b47456c229c76fbb836100
Nigui <guin56@gmail.com>
2013-12-11 18:46:26 +0100

    Revert "Revert """
    This reverts commit 36b7545aea961c1989543447752641b04e29b5df.

--- main.c
+++ main.c
@@ -13,13 +13,13 @@
  */
 int main(int argc, char** argv) {
 
-    printf("Hello world\n!");
     printf("it work\n");
     printf("successfully\n");
     int i;
     for(i=0;i<10;i++){
         printf("%d\n",i);
     }
+    printf("THE END\n");
     return (EXIT_SUCCESS);
 }
 
